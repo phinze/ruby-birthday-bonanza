b1c721f9bb315305b6ea07d864990be69a4609ae
Paul Hinze <phinze@phinze.com>
2024-05-13 12:18:30 -0500

    Simplest ruby server

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = with pkgs;
+          mkShell {
+            packages = [
+              ruby
+            ];
+
+            # Keep gems installed in a subdirectory
+            BUNDLE_PATH = "./vendor/bundle";
+        };
+      });
+}
